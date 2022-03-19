ba4e013a17f441a5462246943616877f789a0d51
Parth Joshi <joshiparth705@gmail.com>
2022-03-19 12:37:02 +0530

    THT-20 --> Home Page Bottom Half 3 parts done

--- pages/home.js
+++ pages/home.js
@@ -4,12 +4,16 @@ import SolidButton from '../components/Common/Buttons/SolidButton';
 import OutlinedButton from '../components/Common/Buttons/OutlinedButton';
 import WhiteButton from '../components/Common/Buttons/WhiteButton';
 import UniqueHobbies from '../components/HomePage/UniqueHobbies';
+import Quiz from '../components/HomePage/Quiz';
+import GlobalInSpirit from '../components/HomePage/GlobalInSpirit';
 
 
 const home = () => {
   return (
     <div>
       <UniqueHobbies />
+      <Quiz />
+      <GlobalInSpirit />
     </div>
   )
 }
