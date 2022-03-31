afb6fc7dfccfcc1cfa9ee58a7242d3a3fb8181d3
Pratinav Shrivastava <pratinavshrivastava6@gmail.com>
2022-03-31 12:57:16 +0530

    About Us filenames changed, images and icon files left

--- components/index.js
+++ components/index.js
@@ -32,7 +32,7 @@ export {default as JoinTheTribe} from '/components/homepage/JoinTheTribe'
 export {default as Testimonial} from '/components/homepage/Testimonial'
 
 // ----------------------------------------------------------------------------- About Us Components ------------------------------------------------------------------------------->
-export {default as Welcome} from '/components/about/Welcome'
-export {default as HowItBegan} from '/components/about/HowItBegan'
-export {default as MeetTheTeam} from '/components/about/MeetTheTeam'
-export {default as TeamRecommends} from '/components/about/TeamRecommends'
+export {default as Welcome} from '/components/about/welcome'
+export {default as HowItBegan} from '/components/about/how-it-began'
+export {default as MeetTheTeam} from '/components/about/meet-the-team'
+export {default as TeamRecommends} from '/components/about/team-recommends'
